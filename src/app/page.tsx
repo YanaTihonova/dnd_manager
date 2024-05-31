be8a2618079efcd843f086545b114e2260c3e138
Yana Tsikhanava <yana.tihonova15@yandex.by>
2024-05-31 17:19:02 +0300

    RFI Add Markdown support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,30 @@
 
 import { Flex, Text, Button } from "@radix-ui/themes";
 
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~~strikethrough~~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| ad | bd |
+|---|---|
+| *a* | b |
+| a | b |
+`;
+
 export default function Home() {
   return (
-    <main>
-      <Flex direction="column" gap="2">
-        <Text>Hello from Radix Themes :)</Text>
-        <Button>Let's go</Button>
-      </Flex>
-    </main>
+    <Flex direction="column" gap="2" align="center">
+      <Text>Hello from Radix Themes :)</Text>
+      <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+    </Flex>
   );
 }
