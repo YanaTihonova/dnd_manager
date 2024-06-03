513c468ba25d8ef52fdc8cba57efe5620fe9778a
Yana Tsikhanava <yana.tihonova15@yandex.by>
2024-06-03 22:58:31 +0300

    RFI-2 Initial edit mode for Text note1

--- src/components/features/TextNote/index.tsx
+++ src/components/features/TextNote/index.tsx
@@ -7,29 +7,52 @@ import {
   Box,
   Flex,
   Heading,
+  TextArea,
+  TextField,
 } from "@radix-ui/themes";
 
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import { markdown } from "@/components/common/constance";
+import { useState } from "react";
 
 export default function TextNote() {
+  const [editMode, useEditMode] = useState(false);
+  const [title, useTitle] = useState("Simple note");
+  const [text, useText] = useState(markdown);
+
   return (
     <Card>
       <Flex justify="between" gap="3">
-        <Heading truncate>Simple note</Heading>
+        {editMode ? (
+          <TextField.Root
+            value={title}
+            onChange={(event) => useTitle(event.target.value)}
+          />
+        ) : (
+          <Heading truncate>{title}</Heading>
+        )}
         <Flex justify="end" gap="3">
-          <Button>
+          <Button onClick={() => useEditMode(!editMode)}>
             <Pencil1Icon />
           </Button>
-          <Button>
+          <Button color="ruby">
             <TrashIcon />
           </Button>
         </Flex>
       </Flex>
       <Separator my="3" size="4" />
-      <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+      {editMode ? (
+        <TextArea
+          style={{ minHeight: "250px" }}
+          resize="vertical"
+          value={text}
+          onChange={(event) => useText(event.target.value)}
+        />
+      ) : (
+        <Markdown remarkPlugins={[remarkGfm]}>{text}</Markdown>
+      )}
     </Card>
   );
 }
