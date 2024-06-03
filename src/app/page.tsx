c550a5fde3676a01759e7c4becf4de9bb13c8db1
Yana Tsikhanava <yana.tihonova15@yandex.by>
2024-06-03 22:33:57 +0300

    RFI-2 Ajust container stucture to better display content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { Flex, Grid } from "@radix-ui/themes";
+import { Container, Flex, Grid } from "@radix-ui/themes";
 
 import TextNote from "@/components/features/TextNote";
 
 export default function Home() {
   return (
-    <Flex direction="column" gap="3">
+    <Container style={{ padding: "15px" }}>
       <TextNote />
-      <TextNote />
-      <TextNote />
-      <TextNote />
-      <TextNote />
-    </Flex>
+    </Container>
   );
 }
